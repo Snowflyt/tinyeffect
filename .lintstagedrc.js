1b49f78825ecff2918a105b19839a1ccb9a3c18a
Snowflyt <gaoge011022@163.com>
2025-01-28 14:24:43 +0800

    🐳 chore(lint): Remove webhint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -7,7 +7,6 @@ const config = {
   "*.{cjs,mjs,cts,mts}":
     "eslint --fix --no-error-on-unmatched-pattern --report-unused-disable-directives-severity error --max-warnings 0",
   "**/*.{json,md}": "prettier --log-level=silent --no-error-on-unmatched-pattern --write",
-  ".hintrc": "prettier --log-level=silent --no-error-on-unmatched-pattern --write --parser json",
 };
 
 export default config;
